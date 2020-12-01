036db5cc470cf03b585e9908aad087edbcc44afc
goheeji <gmlwl0489@daum.net>
2020-12-01 14:29:31 +0900

    first commit

---
+++ JAVA/FAST_JAVA/Practice/src/chapter5/Order.java
@@ -0,0 +1,21 @@
+package chapter5;
+
+public class Order {
+
+	/*
+	 * long orderId;
+	 * String customerId;
+	 * String orderDate; //날짜는 Calendar 클래스를 많이 사용하지만, 여기서는 일단 Strin으로
+	 * String customerName;
+	 * String productId;
+	 * String shippingAddress;
+	
+	*/
+	String id;
+	String userID;
+	String date;
+	String name;
+	String number;
+	String address;
+
+}
