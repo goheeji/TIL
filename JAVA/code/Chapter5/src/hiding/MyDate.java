e73eb8f987ce7f089dd94d27e071d5a2c6e52b5b
goheeji <gmlwl0489@daum.net>
2020-12-02 11:40:48 +0900

    정보은닉 코드 추가

---
+++ JAVA/code/Chapter5/src/hiding/MyDate.java
@@ -0,0 +1,49 @@
+package hiding;
+
+public class MyDate {
+
+	private int day;
+	private int month;
+	private int year;
+	
+	private boolean isValid;
+
+	public void setDay(int day) { // 값을 넣을때
+		this.day = day;
+	}
+
+	public int getDay() { // 값을 가져갈때
+		return day;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		
+		if(month < 1 || month >12){
+			isValid = false;
+		}
+		else
+			this.month = month;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public void showDate() {
+		if(isValid){
+		System.out.println(year + "년" + month + "월" + day + "일");
+		}
+		else{
+			System.out.println("유효하지 않은 날짜 입니다.");
+		}
+	}
+
+}
---
+++ JAVA/code/Chapter5/src/hiding/MyDateTest.java
@@ -0,0 +1,17 @@
+package hiding;
+
+public class MyDateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyDate date = new MyDate();
+
+		date.setYear(2020);
+		date.setMonth(1200);
+		date.setDay(2);
+
+		date.showDate();
+
+	}
+
+}
